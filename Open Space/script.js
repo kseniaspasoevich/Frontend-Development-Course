3675a7fb680e526e0aac2fb51ca467ec32a46d32
kseniaspasoevich <kseniya.spasoyevich@gmail.com>
2022-02-08 22:58:30 +0300

    Rearrange

--- Open Space/script.js
+++ Open Space/script.js
@@ -1,4 +1,61 @@
-function enableControlPanel(){
+function checkPassword() {
+    let password = document.getElementById("password-field");
+    if (password.value === "TrustNo1") {
+        document.querySelectorAll("input").forEach(el => el.removeAttribute("disabled"));
+        password.setAttribute("disabled", "disabled");
+        document.getElementById("ok-button").setAttribute("disabled", "disabled");
+        document.getElementById("launch-button").setAttribute("disabled", "disabled")
+    } else {  alert("WRONG");
+    }
+}
+
+document.getElementById("ok-button").addEventListener("click", checkPassword);
+
+
+function checkCheckboxes() {
+    let checkboxes = document.querySelectorAll("input[type=checkbox]")
+    let count = 0;
+    for (const checkbox in checkboxes) {
+        if (checkboxes[checkbox].checked) {
+            count ++;
+        }
+    }
+    let ok = (count == 6) ? true : false;
+
+    return ok;
+}
+
+function checkSliders() {
+    let sliders = document.querySelectorAll("input[type=range]");
+    let count = 0;
+    for (const slider in sliders) {
+        if (sliders[slider].value==100) {
+            count ++;
+        }
+    }
+    let ok = (count == 5) ? true : false;
+    return ok;
+}
+
+
+function checkForLaunch() {
+    if (checkCheckboxes() && checkSliders()) {
+        document.getElementById("launch-button").removeAttribute("disabled");
+        document.getElementById("launch-button").addEventListener("click", launch);
+    }
+}
+
+function launch() {
+    document.getElementById("rocket").animate([
+        // keyframes
+        { transform: 'translateY(-300px)' }
+    ], {
+        // timing options
+        duration: 3000,
+        iterations: 1
+    });
+}
+/*function enableControlPanel(){
     let psd = document.getElementById("text-box");
     let password = document.getElementById("text-box").value;
 
@@ -54,4 +111,12 @@ function launchRocket(){
         duration: 3000,
         iterations: 1
     });
-}
+}*/
+
+
+
+
+
+
+
+
